32e94a06f47e074f4072a9d3b647781d9a19ac20
Pavel Vilbik <p.vilbik@softteco.com>
2014-03-20 15:54:17 +0300

    refactoring

--- QuoteApp/Classes/View Controllers/QuoteOrderModalViewController.m
+++ QuoteApp/Classes/View Controllers/QuoteOrderModalViewController.m
@@ -60,45 +60,46 @@
 }
 
 - (void)additionalButtonAction{
-        Order *order = [[Order alloc] init];
+    Order *order = [[Order alloc] init];
     
-        order.referenceNumber = self.item.referenceNumber;
-        order.originator = self.item.originator;
-        order.originator = self.item.originator;
-        order.activeUsers = self.item.activeUsers;
-        order.businessLogicScripts = self.item.businessLogicScripts;
-        order.scheduledBusinessLogic = self.item.scheduledBusinessLogic;
-        order.collaborators = self.item.collaborators;
-        order.backendEnvironments = self.item.backendEnvironments;
-        order.dataStorage = self.item.dataStorage;
-        order.businessLogicExecutionTimeLimit = self.item.businessLogicExecutionTimeLimit;
-        order.startSubscriptionDate = self.item.startSubscriptionDate;
-        order.totalPrice = self.item.totalPrice;
-        order.product = self.item.product;
-        order.currentSubscription = STARTER_ORDER_SUBSCRIPTION;
-        
-        [[DataHelper instance] saveOrder:order
-                               OnSuccess:^(NSArray *orders){
-                                   if (orders.count) {
-                                       self.item = [orders firstObject];
-                                       UIAlertView *alert = [[UIAlertView alloc] initWithTitle:LOC(DV_MESSAGE_ORDER_CONFIRMATION_TITLE)
-                                                                                       message:LOC(DV_MESSAGE_ORDER_CONFIRMATION_TEXT)
-                                                                                      delegate:self
-                                                                             cancelButtonTitle:LOC(OKAY)
-                                                                             otherButtonTitles:nil];
-                                       [alert show];
-                                   }
-                               }
-                               onFailure:^(NSError *error){
-                                   UIAlertView *alert = [[UIAlertView alloc] initWithTitle:LOC(DV_MESSAGE_ERROR_CREATE_ORDER)
-                                                                                   message:error.localizedDescription
-                                                                                  delegate:nil
+    order.meta = [[KCSMetadata alloc] init];
+    order.referenceNumber = self.item.referenceNumber;
+    order.originator = self.item.originator;
+    order.originator = self.item.originator;
+    order.activeUsers = self.item.activeUsers;
+    order.businessLogicScripts = self.item.businessLogicScripts;
+    order.scheduledBusinessLogic = self.item.scheduledBusinessLogic;
+    order.collaborators = self.item.collaborators;
+    order.backendEnvironments = self.item.backendEnvironments;
+    order.dataStorage = self.item.dataStorage;
+    order.businessLogicExecutionTimeLimit = self.item.businessLogicExecutionTimeLimit;
+    order.startSubscriptionDate = self.item.startSubscriptionDate;
+    order.totalPrice = self.item.totalPrice;
+    order.product = self.item.product;
+    order.currentSubscription = STARTER_ORDER_SUBSCRIPTION;
+    
+    [[DataHelper instance] saveOrder:order
+                           OnSuccess:^(NSArray *orders){
+                               if (orders.count) {
+                                   self.item = [orders firstObject];
+                                   UIAlertView *alert = [[UIAlertView alloc] initWithTitle:LOC(DV_MESSAGE_ORDER_CONFIRMATION_TITLE)
+                                                                                   message:LOC(DV_MESSAGE_ORDER_CONFIRMATION_TEXT)
+                                                                                  delegate:self
                                                                          cancelButtonTitle:LOC(OKAY)
                                                                          otherButtonTitles:nil];
                                    [alert show];
                                }
-         ];
-        
+                           }
+                           onFailure:^(NSError *error){
+                               UIAlertView *alert = [[UIAlertView alloc] initWithTitle:LOC(DV_MESSAGE_ERROR_CREATE_ORDER)
+                                                                               message:error.localizedDescription
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:LOC(OKAY)
+                                                                     otherButtonTitles:nil];
+                               [alert show];
+                           }
+     ];
+    
 }
 
 
