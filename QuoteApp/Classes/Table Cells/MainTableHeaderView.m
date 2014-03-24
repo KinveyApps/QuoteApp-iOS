4260530544a22a0fed4aa2b515ec49f523bc2b9f
Pavel Vilbik <p.vilbik@softteco.com>
2014-03-24 15:15:39 +0300

    add comments to custom view

--- QuoteApp/Classes/Table Cells/MainTableHeaderView.m
+++ QuoteApp/Classes/Table Cells/MainTableHeaderView.m
@@ -3,12 +3,21 @@
 //  QuoteApp
 //
 //  Created by Pavel Vilbik on 11.2.14.
-//  Copyright (c) 2014 Kinvey, Inc. All rights reserved.
-//
+/**
+ * Copyright (c) 2014 Kinvey Inc. *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at *
+ * http://www.apache.org/licenses/LICENSE-2.0 *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License. *
+ */
 
 #import "MainTableHeaderView.h"
 
 @interface MainTableHeaderView ()
+
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UILabel *noLabel;
@@ -29,6 +38,7 @@
 		[self.contentView addSubview:self.view];
 		self.view.frame = self.contentView.bounds;
     }
+    
     return self;
 }
 
--- QuoteApp/Classes/Table Cells/ProductTableViewCell.m
+++ QuoteApp/Classes/Table Cells/ProductTableViewCell.m
@@ -3,8 +3,16 @@
 //  QuoteApp
 //
 //  Created by Pavel Vilbik on 14.2.14.
-//  Copyright (c) 2014 Kinvey, Inc. All rights reserved.
-//
+/**
+ * Copyright (c) 2014 Kinvey Inc. *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at *
+ * http://www.apache.org/licenses/LICENSE-2.0 *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License. *
+ */
 
 #import "ProductTableViewCell.h"
 #import <KinveyKit/KinveyKit.h>
@@ -16,7 +24,6 @@
 @property (weak, nonatomic) IBOutlet UIView *descriptionView;
 @property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
 @property (weak, nonatomic) IBOutlet KinveyImageView *kinveyImageView;
-
 
 @end
 
@@ -33,6 +40,7 @@
 		self.view.frame = self.contentView.bounds;
         self.descriptionView.layer.cornerRadius = 5.0f;
     }
+    
     return self;
 }
 
