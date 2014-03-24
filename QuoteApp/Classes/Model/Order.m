68e1c55db59888ea06e2d33fa60b4e5c68ebcacb
Pavel Vilbik <p.vilbik@softteco.com>
2014-03-24 15:07:13 +0300

    Add comments for model classes

--- QuoteApp/Classes/Model/Order.m
+++ QuoteApp/Classes/Model/Order.m
@@ -3,8 +3,17 @@
 //  QuoteApp
 //
 //  Created by Igor Sapyanik on 6.2.14.
-//  Copyright (c) 2014 Kinvey, Inc. All rights reserved.
-//
+/**
+ * Copyright (c) 2014 Kinvey Inc. *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at *
+ * http://www.apache.org/licenses/LICENSE-2.0 *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License. *
+ */
+
 
 #import "Order.h"
 
@@ -12,10 +21,12 @@
 
 - (NSDictionary *)hostToKinveyPropertyMapping
 {
-    
+    //Kinvey: Mapping Function
     NSMutableDictionary *mapping = [NSMutableDictionary dictionary];
     [mapping addEntriesFromDictionary:[super hostToKinveyPropertyMapping]];
-    [mapping addEntriesFromDictionary:@{ @"currentSubscription" : @"currentSubscription" }];
+    //                                  client property         : backend column
+    //                                  ------------------------:-------------------------------
+    [mapping addEntriesFromDictionary:@{ @"currentSubscription" : @"currentSubscription" }];    //currentSubscription maps to currentSubscription
     return mapping;
 }
 
--- QuoteApp/Classes/Model/Quote.m
+++ QuoteApp/Classes/Model/Quote.m
@@ -3,8 +3,17 @@
 //  QuoteApp
 //
 //  Created by Igor Sapyanik on 6.2.14.
-//  Copyright (c) 2014 Kinvey, Inc. All rights reserved.
-//
+/**
+ * Copyright (c) 2014 Kinvey Inc. *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at *
+ * http://www.apache.org/licenses/LICENSE-2.0 *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License. *
+ */
+
 
 #import "Quote.h"
 
@@ -12,25 +21,28 @@
 
 - (NSDictionary *)hostToKinveyPropertyMapping
 {
-    return @{@"kinveyId"                        : KCSEntityKeyId,
-			 @"meta"                            : KCSEntityKeyMetadata,
-             @"originator"                      : @"originator",
-             @"referenceNumber"                 : @"referenceNumber",
-             @"activeUsers"                     : @"activeUsers",
-             @"businessLogicScripts"            : @"businessLogicScripts",
-             @"scheduledBusinessLogic"          : @"scheduledBusinessLogic",
-             @"collaborators"                   : @"collaborators",
-             @"backendEnvironments"             : @"backendEnvironments",
-             @"dataStorage"                     : @"dataStorage",
-             @"businessLogicExecutionTimeLimit" : @"businessLogicExecutionTimeLimit",
-             @"totalPrice"                      : @"totalPrice",
-             @"startSubscriptionDate"           : @"startSubscriptionDate",
-             @"product"                         : @"product"};
+    //Kinvey: Mapping Function
+    //      client property                     : backend column
+    //      ------------------------------------:---------------------------------------
+    return @{@"kinveyId"                        : KCSEntityKeyId,                       //kinveyId maps to _id
+			 @"meta"                            : KCSEntityKeyMetadata,                 //meta maps to metadata
+             @"originator"                      : @"originator",                        //originator maps to originator
+             @"referenceNumber"                 : @"referenceNumber",                   //referenceNumber maps to referenceNumber
+             @"activeUsers"                     : @"activeUsers",                       //activeUsers maps to activeUsers
+             @"businessLogicScripts"            : @"businessLogicScripts",              //businessLogicScripts maps to businessLogicScripts
+             @"scheduledBusinessLogic"          : @"scheduledBusinessLogic",            //scheduledBusinessLogic maps to scheduledBusinessLogic
+             @"collaborators"                   : @"collaborators",                     //collaborators maps to collaborators
+             @"backendEnvironments"             : @"backendEnvironments",               //backendEnvironments maps to backendEnvironments
+             @"dataStorage"                     : @"dataStorage",                       //dataStorage maps to dataStorage
+             @"businessLogicExecutionTimeLimit" : @"businessLogicExecutionTimeLimit",   //businessLogicExecutionTimeLimit maps to businessLogicExecutionTimeLimit
+             @"totalPrice"                      : @"totalPrice",                        //totalPrice maps to totalPrice
+             @"startSubscriptionDate"           : @"startSubscriptionDate",             //origistartSubscriptionDatenator maps to startSubscriptionDate
+             @"product"                         : @"product"};                          //product maps to product
 }
 
 + (NSDictionary *)kinveyPropertyToCollectionMapping{
     return @{ @"originator" : KCSUserCollectionName,
-              @"product" : @"Products"};
+              @"product"    : @"Products"};
 }
 
 + (NSDictionary *)kinveyObjectBuilderOptions{
